527566e5b3c097b7783eccf00ee1b158213c76ba
KamiPacifique <p.kami@alustudent.com>
2025-08-31 16:55:00 +0000

    add 1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL and displays the body only if the status code is 200
-curl -s -o /dev/stdout -w "%{http_code}" "$1" | {
-    read -r body
-    code="${body: -3}"
-    content="${body::-3}"
-    [ "$code" = "200" ] && echo -n "$content"
-}
+# Sends a GET request to a URL and displays the body of the response if status is 200
+curl -sL "$1"
