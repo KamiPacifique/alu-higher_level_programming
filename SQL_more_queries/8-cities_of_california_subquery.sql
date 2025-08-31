ec43e62119c0ab054784eb186c13ba50a27b4b5a
KamiPacifique <p.kami@alustudent.com>
2025-08-31 08:03:53 -0700

    add 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,7 @@
+-- Write a script that lists all the cities of California in the database hbtn_0d_usa
+-- Do not use JOIN, use a subquery instead
+-- Results sorted by cities.id in ascending order
+
 SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
