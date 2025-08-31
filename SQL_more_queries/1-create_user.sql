fdc158b1b33181015f2c54c7050430136fdf2a52
KamiPacifique <p.kami@alustudent.com>
2025-08-31 07:44:21 -0700

    add 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1-create_user.sql
+-- Creates user_0d_1@localhost with full privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
