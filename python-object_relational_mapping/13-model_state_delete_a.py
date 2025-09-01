4368aafd313b12f3a5c85bc8fbce945f3c93f276
KamiPacifique <p.kami@alustudent.com>
2025-09-01 12:50:15 -0700

    add 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     session = Session()
     Base.metadata.create_all(engine)
 
-    states = session.query(State).filter(State.name.contains('a')).all()
+    states = session.query(State).filter(State.name.contains('%a%')).all()
 
     for state in states:
         session.delete(state)
