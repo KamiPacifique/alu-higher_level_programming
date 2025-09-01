74d4835c12da5edc445d83dffde001b41105e631
KamiPacifique <p.kami@alustudent.com>
2025-09-01 16:21:01 +0000

    add 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetch a URL and display the response body info using urllib."""
 import urllib.request
 
-url = "http://0.0.0.0:5050/status"
+url = "https://alu-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
